8c2bbc3999ac634013f9ab94e43c588cf62b3572
Anshika Agrawal <anshikaagrawal5501@gmail.com>
2020-07-08 15:17:39 +0530

    adding codes

---
+++ Basic Part-I/code10.java
@@ -0,0 +1,16 @@
+/**
+Write a Java program to compute a specified formula.
+Specified Formula :
+4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+
+ */
+
+import java.io.*;
+class code10
+{
+    public static void main ()
+    {
+        System.out.println ("4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)) = "+(4.0*(1-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)))
+        );
+    }
+}
---
+++ Basic Part-I/code9.java
@@ -0,0 +1,14 @@
+/** Write a Java program to compute the specified expressions and print the output
+ * 
+ * ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+ */
+
+import java.io.*;
+class code9
+{
+    public static void main ()
+    {
+        double d=((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println ("((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) = "+d);
+    }
+}
