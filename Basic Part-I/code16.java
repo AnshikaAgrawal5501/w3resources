12f6fee1ae6ab5e759bb92eae855e641f8983615
Anshika Agrawal <anshikaagrawal5501@gmail.com>
2020-07-08 15:50:48 +0530

    adding codes

---
+++ Basic Part-I/code16.java
@@ -0,0 +1,14 @@
+//Write a Java program to print a face.
+
+import java.io.*;
+class code16
+{
+    public static void main ()
+    {
+        System.out.println (" +\"\"\"\"\"+ ");
+        System.out.println ("[| o o |]");
+        System.out.println (" |  ^  |");
+        System.out.println (" | '-' |");
+        System.out.println (" +-----+");
+    }
+}
