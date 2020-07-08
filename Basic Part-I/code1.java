1630f356d834c28e6b2ee584177dc75f598158ec
Anshika Agrawal <anshikaagrawal5501@gmail.com>
2020-07-08 15:06:52 +0530

    adding codes

---
+++ Basic Part-I/code1.java
@@ -0,0 +1,11 @@
+//Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+
+import java.io.*;
+class code1
+{
+    public static void main ()
+    {
+        System.out.println ("Hello");
+        System.out.println ("Anshika Agrawal");
+    }
+}
